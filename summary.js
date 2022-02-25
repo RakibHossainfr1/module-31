479838de366e6ca7425bbca1dc24b76e7e88e6b8
RakibHossainfr1 <rakibhossain.fr@gmail.com>
2022-02-25 16:02:25 +0600

    add summary and end Module 31

--- summary.js
+++ summary.js
@@ -2,6 +2,25 @@
 // const ar value poriborton kora jay nah but let poriborton kora jay
 let age = 23;
 age = 24;
-console.log(age)
+// console.log(age);
 
 // kono function ar paramiter defult babe rahte hole 
+
+const calc = function(x, y, z=0){
+    return x + y + z;
+}
+
+// console.log(calc(23, 23, 80))
+
+// Temlate String
+const name = 'rakib';
+const fly = '3:50';
+const add = `Ami ${name}. alhamdulillah Inshaallah ${fly} at date.`
+// console.log(add)
+
+// Arrow function 
+const equall = (number1, number2) => {
+    return number1 * number2;
+}
+
+// console.log(equall(34, 34)); 

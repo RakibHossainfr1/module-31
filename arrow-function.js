077b50157169dce6e6bd72217ef59849bcc418ea
RakibHossainfr1 <rakibhossain.fr@gmail.com>
2022-02-25 12:25:25 +0600

    summary

--- arrow-function.js
+++ arrow-function.js
@@ -1,4 +1,23 @@
+// arrow function
 const add2 = function (num1, num2){
     return num1 + num2;
 }
-console.log(add2(23, 35));
+// console.log(add2(23, 35));
+// arrow function
+const add4 = (num1, num2) => num1 + num2;
+// console.log(add4(24, 56))
+
+// arrow function 
+const add5 = (name1, name2) => {
+    const sum = name1 + name2;
+    const sub = sum + name1;
+    return sub;
+}
+
+// console.log(add5(23, 46))
+// Spreed (...)
+const age = [23, 34, 54, 65, 34];
+const addAges = [44, 23, ...age, 67]
+console.log(...addAges)
+
+// 
---
+++ summary.js
@@ -0,0 +1,7 @@
+// const not ReAssign but let ReAssing 
+// const ar value poriborton kora jay nah but let poriborton kora jay
+let age = 23;
+age = 24;
+console.log(age)
+
+// kono function ar paramiter defult babe rahte hole 
